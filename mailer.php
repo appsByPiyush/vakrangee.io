fa904e19f51fed603962561a0c2da58bd2117fe2
Piyush Suhalka <piyushsuhalkaa@gmail.com>
2018-07-18 00:19:55 +0530

    Upadte-1807180019

--- mailer.php
+++ mailer.php
@@ -20,7 +20,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "support@rstheme.com";
+        $recipient = "piyushsuhalka@gmail.com";
 
         // Set the email subject.
         $subject = "Edulearn - New contact from $name";
